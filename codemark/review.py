e3b402f7139af62191e27e1e2cb0e10e84dedcce
Lab Incorporation <dev.frolics@gmail.com>
2023-04-23 02:55:34 +0530

    Fixed some bugs with Code review generation

--- codemark/review.py
+++ codemark/review.py
@@ -18,14 +18,14 @@ def reviewCode():
      
 
 
-def generate_query_and_response(programCode, questionTitle, questionDescription, expectedInput, expectedOutput):
-    query = "Review the following code given below, written in C. But, in no way you have to give the correct answer. Don't start with something like Answer: or ChatGPT: or Solution: etc, just plain solution."\
+def generate_query_and_response(programCode, questionTitle, questionDescription, expectedInput, expectedOutput, extension="c"):
+    query = "Review the following code given below, written in {5}. But, in no way you have to give the correct answer. Don't start with something like Answer: or ChatGPT: or Solution: etc, just plain solution."\
             "Check for any compile time as well possible runtime errors."\
             " Logical error based on question statement. As well recommend better algorithm to improve the code what don't give the corrected code."\
-            "\n\nProgram Code: \n\n{0}\n\n\nQuestion: \n\n{1}\n\n{2}\n\nExpected Input:\n\n{3}\n\nExpected Output:\n\n{4}".format(programCode, questionTitle, questionDescription, expectedInput, expectedOutput)
+            "\n\nProgram Code: \n\n{0}\n\n\nQuestion: \n\n{1}\n\n{2}\n\nExpected Input:\n\n{3}\n\nExpected Output:\n\n{4}".format(programCode, questionTitle, questionDescription, expectedInput, expectedOutput, extension)
     
     response_text = response(query)
-    return speaker_remove.sub("", response_text)
+    return "RESPONSE: " + speaker_remove.sub("", response_text).strip().replace("\n\n", "\n")
 
 def response(query):
     prompt=query

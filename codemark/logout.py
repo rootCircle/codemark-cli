dabe070eb7fc368cec1f42b4a27c79b39151599e
Lab Incorporation <dev.frolics@gmail.com>
2023-04-24 22:33:33 +0530

    Logout will now erase current user info from pauth

--- codemark/logout.py
+++ codemark/logout.py
@@ -3,6 +3,9 @@ import codemark.utils
 import codemark.account
 import codemark.initialise
 import os
+import codemark.firebase.database as FireDB
+
+db = FireDB.FirebaseDB()
 
 def logout():
     # Replace 'service_name' and 'account_name' with your own values
@@ -16,5 +19,7 @@ def logout():
 
     if os.path.isfile(codemark.initialise.ACCOUNT_DATA_LOC):
         os.remove(codemark.initialise.ACCOUNT_DATA_LOC)
+
+    db.signout()
 
     print("Logout successful!")

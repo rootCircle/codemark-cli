9dd75563ecf6ee3642af2e5b6029e780dba0dcd0
Lab Incorporation <dev.frolics@gmail.com>
2023-04-27 01:26:05 +0530

    Added alias for codemark for terminal.
    Now cmk --help will work the same as codemark --help
    Submitting assignments are now more easier

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='codemark',
-    version='0.2.0',
+    version='0.2.1',
     description='CodeMark CLI is a command-line interface that helps you manage your coding assignments and tests.\nWith this tool, you can easily initialize the configuration, list assignments, fetch and check your code, submit your code for grading, and get AI-powered error recommendations.',
     author='rootCircle',
     url='https://github.com/rootCircle/codemark-cli',
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'codemark = codemark.scripts.cm_cli:cli',
+            'cmk = codemark.scripts.cm_cli:cli',
         ],
     },
     package_data={'codemark': ['firebase/res/service-account-file.json']}
